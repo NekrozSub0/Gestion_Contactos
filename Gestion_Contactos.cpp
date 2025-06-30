5d57ebc9580236900012414ccf0f6c55cecd8bbf
Alexander <alex-c-r@hotmail.es>
2025-06-30 10:46:33 -0500

    Avance: Agregado de menú en bucle con validación de opcion

--- Gestion_Contactos.cpp
+++ Gestion_Contactos.cpp
@@ -13,31 +13,40 @@ struct contactoEmail{
 	
 int main(){
 	char menu_opcion;
+	do{
 	cout<<"Ingrese una opcion"<<endl;
 	cout<<"a) Agregar un contacto"<<endl;
 	cout<<"b) Eliminar un contacto"<<endl;
 	cout<<"c) Mostrar lista de contactos registrados"<<endl;
 	cout<<"d) Mostrar contactos existentes, ordenado por correo"<<endl;
-	cout<<"e) Salir del programa"<<endl;
+	cout<<"e) Salir del programa"<<endl<<endl;
 	cin>>menu_opcion;
 	switch (menu_opcion){
 		case 'a':
+			//Agregar un contacto
 			
 			break;
 		case 'b':
+			//Eliminar un contacto
 			
 			break;
 		case 'c':
+			//Mostrar lista de contactos registrados
 			
 			break;
 		case 'd':
+			//Mostrar contactos existentes, ordenado por correo
 			
 			break;
 		case 'e':
-			
+			//Salir del programa
+			cout<<"Programa finalizado"<<endl;
 			break;	
 		default:
+			//Repetir menu hasta elegir una opcion
+			cout<<"ERROR: Eliga una opcion del menu (a, b, c, d, e)"<<endl<<endl;
 			break;
 	}
+	}while (menu_opcion!='e');
 	return 0;
 }
