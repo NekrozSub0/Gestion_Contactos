098e558883851841233a1abe8b1562cfa326c45c
Alexander <alex-c-r@hotmail.es>
2025-06-30 10:23:25 -0500

    Avance: Creación de estructura contactoEmail

--- Gestion_Contactos.cpp
+++ Gestion_Contactos.cpp
@@ -2,3 +2,17 @@
 #include <string>
 using namespace std;
 
+struct contactoEmail{
+	string nombre;
+	char sexo;
+	int edad;
+	string telefono;
+	string email;
+	string nacionalidad;
+};
+	
+int main(){
+
+	
+	return 0;
+}
