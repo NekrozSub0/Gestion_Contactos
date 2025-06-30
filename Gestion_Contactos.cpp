43b1ba4aabc5ba28354d8629f3ee5d237664f2be
Alexander <alex-c-r@hotmail.es>
2025-06-30 10:37:21 -0500

    Avance: Estructuración del menú de opciones con switch

--- Gestion_Contactos.cpp
+++ Gestion_Contactos.cpp
@@ -12,7 +12,32 @@ struct contactoEmail{
 };
 	
 int main(){
-
-	
+	char menu_opcion;
+	cout<<"Ingrese una opcion"<<endl;
+	cout<<"a) Agregar un contacto"<<endl;
+	cout<<"b) Eliminar un contacto"<<endl;
+	cout<<"c) Mostrar lista de contactos registrados"<<endl;
+	cout<<"d) Mostrar contactos existentes, ordenado por correo"<<endl;
+	cout<<"e) Salir del programa"<<endl;
+	cin>>menu_opcion;
+	switch (menu_opcion){
+		case 'a':
+			
+			break;
+		case 'b':
+			
+			break;
+		case 'c':
+			
+			break;
+		case 'd':
+			
+			break;
+		case 'e':
+			
+			break;	
+		default:
+			break;
+	}
 	return 0;
 }
